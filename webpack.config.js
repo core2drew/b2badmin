aa16e5ed2900db1f2f5e1934f50a79332fa9b71d
Sandro Calupe <sandrocalupe@Sandros-iMac.local>
2019-05-08 21:35:56 +0800

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/sass/index.scss',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'resolve-url-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              config: {
+                path: './'
+              }
+            }
+          },
+          'sass-loader',
+        ],
+        include: path.resolve(__dirname, '../')
+      }
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '/css/styles.css'
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  resolve: {
+    alias: {
+      Variables: path.resolve(__dirname, 'src/sass/variables.scss'),
+      Helpers: path.resolve(__dirname, 'src/sass/helpers.scss')
+    }
+  }
+};
